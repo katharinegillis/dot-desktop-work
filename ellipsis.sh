a397fcd8e2e4038774649cbb7ee5c26d24110312
Katharine Cordes <gillis.katharine@gmail.com>
2020-02-02 01:03:38 -0700

    Add pull and update packages

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+packages=(
+    common
+);
+
 pkg.install() {
-    packages=(
-        thomshouse-ellipsis/zsh
-    );
-    
-    for package in $packages; do
+    pkg.pull
+}
+
+pkg.pull() {
+    for package in ${packages[*]}; do
         ellipsis.list_packages | grep "$ELLIPSIS_PACKAGES/$package" 2>&1 > /dev/null;
         if [ $? -ne 0 ]; then
             $ELLIPSIS_PATH/bin/ellipsis install $package;
